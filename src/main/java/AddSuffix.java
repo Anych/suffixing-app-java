989275472662aa7757a5615660b93eb5fc819f3c
Anych <anuar123@mail.ru>
2022-08-17 10:06:29 +0600

    Add the the get files properties and start to do logging

--- src/main/java/AddSuffix.java
+++ src/main/java/AddSuffix.java
@@ -1,36 +1,51 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.logging.*;
 
 public class AddSuffix {
-    static String configPath = "D:\\java\\suffixing-app\\src\\test\\resources\\sandbox\\config\\three-files-config.properties";
+    private static final Logger logger = Logger.getLogger("AddSuffix");
+    static String configPath;
     static String mode;
     static String suffix;
     static String[] files;
 
     public static void main(String[] args) {
+        configPath = args[0];
+
+        Properties properties = readFile();
+        if (properties != null) {
+            getFilesProperties(properties);
+        }
+    }
+
+    public static Properties readFile() {
         try (InputStream input = new FileInputStream(configPath)) {
 
             Properties prop = new Properties();
             prop.load(input);
-
-            getFilesProperties(prop);
-            files = prop.getProperty("files").split(":");
-            System.out.println(mode);
-            System.out.println(suffix);
-            System.out.println(Arrays.toString(files));
+            return prop;
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        return null;
     }
 
     public static void getFilesProperties(Properties prop) {
         mode = prop.getProperty("mode");
+        recognizeMode(mode);
         suffix = prop.getProperty("suffix");
         files = prop.getProperty("files").split(":");
+    }
+
+    public static void recognizeMode(String mode) {
+        if (!mode.toLowerCase().equals("copy") && !mode.toLowerCase().equals("move")) {
+            logger.log(Level.SEVERE, "Mode is not recognized: " + mode);
+            System.exit(0);
+        }
     }
 
 }
